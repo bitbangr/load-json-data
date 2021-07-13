fa918070b9824d711b493f4b879d9dfe3f260554
bitbangr <mgj000@hotmail.com>
2021-07-13 13:27:23 -0700

    initial working boilerplate

---
+++ src/Colors.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export const Colors = () => {
+  return (
+    <>
+      <div className="color-container">Welcome to Color Swatches</div>
+    </>
+  );
+};
