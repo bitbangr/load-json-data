535aff35526759c8f8cb195e874613091ef4ef03
bitbangr <mgj000@hotmail.com>
2021-07-13 13:54:22 -0700

    Read/Display Json file
    Simple display of top level info from Json file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import colorData from './kroma_colors.json';
 import {Colors} from "./Colors";
 
 function App() {
--- src/Colors.js
+++ src/Colors.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./App.css";
+import colorData from './kroma_colors.json';
 
 export const Colors = () => {
   return (
     <>
-      <div className="color-container">Welcome to Color Swatches</div>
+      <div className="color-container">
+          {colorData.map((data,key) => {
+            return (
+              <div key={key}>
+                {data.name +
+                ' , ' +
+                data.url +
+                ' ,' +
+                data.description}
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
