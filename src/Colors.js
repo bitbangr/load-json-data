31a1d6fe2f2ab9a7f0859db15ab7df4d7e554791
bitbangr <mgj000@hotmail.com>
2021-07-14 01:33:53 -0700

    code updates

--- src/Colors.js
+++ src/Colors.js
@@ -1,33 +1,95 @@
 import React from "react";
 import "./App.css";
+// import colorData from './kroma_colors_simple.json';
 import colorData from './kroma_colors.json';
 
 export const Colors = () => {
   return (
     <>
-      <h1> JSON Color Data</h1>
+      <HomePageHeader />
       <div className="color-container">
           {colorData.map((data,key) => {
             return (
               <div key={key}>
-                {data.name +
-                ' , ' +
-                data.url +
-                ' ,' +
-                data.description}
-                {data.colors.map((sub, subindex) =>
-                    <p key={subindex}>{sub.name +
-                      ', [' +
-                    sub.rgb +
-                    '], ' +
-                    sub.number
-                    }</p>)
-                }
+              <Color
+                key={key}
+                col_name={data.name}
+                url={data.url}
+                description={data.description}
+              />
               </div>
-
             );
           })}
       </div>
     </>
+  );
+};
+
+export const ColorSimple = () => {
+  return (
+    <>
+      <HomePageHeader />
+      <div className="color-container">
+          {colorData.map((data,key) => {
+            return (
+              <div key={key}>
+              <Color
+                key={key}
+                col_name={data.name}
+                url={data.url}
+                description={data.description}
+              />
+              </div>
+
+
+
+            );
+          })}
+      </div>
+      <div>
+        {colorData.colors.map((sub, subindex) =>
+           return (
+              <p key={subindex}>{sub.name +
+                                    ', [' +
+                                  sub.rgb +
+                                    '], ' +
+                                sub.number } </p>);
+        }
+      </div>
+    </>
+  );
+};
+
+
+const HomePageHeader = () => {
+  return (
+    <header className="header">
+      <h2>JSON Color Data</h2>
+    </header>
+  );
+};
+
+const Color = ({ col_name, url, description }) => {
+  if (!col_name) return <div />;
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <h5>{col_name}</h5>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h5>{url}</h5>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h4>{description}</h4>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
